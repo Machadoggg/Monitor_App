40e24a9a7fb5df8fe5ff625ab13226c583156141
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-01 22:21:04 -0500

    Add Main Page

--- Monitor_App.Movil/MainPage.xaml.cs
+++ Monitor_App.Movil/MainPage.xaml.cs
@@ -1,25 +1,60 @@
-﻿namespace Monitor_App.Movil
+﻿using Monitor_App.Movil.Services;
+
+namespace Monitor_App.Movil
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        //int count = 0;
+        private readonly LocationService _locationService;
+        private const string ApiUrl = "https://tumvcapi.com/api/locations"; // Cambiar la URL
 
         public MainPage()
         {
             InitializeComponent();
+            _locationService = new LocationService();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+
+        private async void OnStartClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await CheckAndRequestLocationPermission();
+            await _locationService.StartSendingLocation(ApiUrl, TimeSpan.FromSeconds(30));
+            StatusLabel.Text = "Enviando ubicación...";
         }
+
+        private void OnStopClicked(object sender, EventArgs e)
+        {
+            _locationService.StopSendingLocation();
+            StatusLabel.Text = "Detenido";
+        }
+
+        private async Task CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+                if (status != PermissionStatus.Granted)
+                {
+                    await DisplayAlert("Permiso requerido", "Se necesita permiso de ubicación para esta función", "OK");
+                }
+            }
+        }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 
 }
