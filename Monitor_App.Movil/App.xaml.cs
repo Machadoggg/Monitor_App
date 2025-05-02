d1c43c3a198d495583697365817741c75ffae35f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-01 22:05:31 -0500

    Add project files.

---
+++ Monitor_App.Movil/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Monitor_App.Movil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Monitor_App.Movil/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Monitor_App.Movil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
