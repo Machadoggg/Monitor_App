d7d3f55790d46091a091d0e21a81eaf822cf10a5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-04 11:29:01 -0500

    Add Api controller Location

---
+++ Monitor_App.Web/Controllers/Api/LocationsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Monitor_App.Web.Models;
+
+namespace Monitor_App.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostLocation([FromBody] LocationData locationData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Locations.Add(locationData);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = locationData.Id }, locationData);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LocationData>>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        [HttpGet("{deviceId}")]
+        public async Task<ActionResult<IEnumerable<LocationData>>> GetDeviceLocations(string deviceId)
+        {
+            return await _context.Locations
+                .Where(l => l.DeviceId == deviceId)
+                .OrderByDescending(l => l.Timestamp)
+                .ToListAsync();
+        }
+    }
+}
