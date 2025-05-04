8fe982ec01bb66ac8b9ad15d156cb9ec8ee5cb84
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-04 11:22:53 -0500

    Add MVC project

---
+++ Monitor_App.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Monitor_App.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
