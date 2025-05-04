15c6b99f3ec2588b962d1039f9f76e9d0ff6d5d7
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-04 12:41:27 -0500

    Add CORS MVC project

--- Monitor_App.Web/Program.cs
+++ Monitor_App.Web/Program.cs
@@ -3,6 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +27,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
