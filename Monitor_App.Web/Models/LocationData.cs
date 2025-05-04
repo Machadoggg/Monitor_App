cda8ba7fb05a05dba6e98cc9b5bbbc122a59f189
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-05-04 11:24:18 -0500

    Add LocationData model

---
+++ Monitor_App.Web/Models/LocationData.cs
@@ -0,0 +1,14 @@
+﻿namespace Monitor_App.Web.Models
+{
+    public class LocationData
+    {
+        public int Id { get; set; }
+        public string DeviceId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double? Altitude { get; set; }
+        public double? Accuracy { get; set; }
+        public double? Speed { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
